364b1b9c5f01ccbde877e40c23650dfc5302b3d7
jspark <sylvan0212@gmail.com>
2021-08-25 20:09:41 +0900

    finished 47

--- section4/conditions.sh
+++ section4/conditions.sh
@@ -9,8 +9,8 @@ fi
 echo "continue with our program"
 echo "bye"
 
-# 3 -eq 3   3 = 3
-# 3 -ne 4   3 is not 4
+# 3 -eq 3   3 == 3
+# 3 -ne 4   3 != 4
 # 3 -gt 1   3 > 1
 # 3 -lt 7   3 < 7
 # 3 -ge 3   3 >= 3
---
+++ section4/conditions2.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash
+
+
+read -p "Type a integer number between 1 and 4: " num
+
+if [ $num == "1" ]; then
+    echo "typed 1"
+elif [ $num == "2" ]; then
+    echo "typed 2"
+elif [ $num == "3" ]; then
+    echo "typed 3"
+elif [ $num == "4" ]; then
+    echo "typed 4"
+else
+    echo "none of the above"
+fi
+
+# read -p "How old are you?" age
+
+# if [ $age -gt 100 ]; then
+#     echo "you are not very young"
+# else
+#     echo "you are still very young"
+# fi
+
+# echo "bye"
