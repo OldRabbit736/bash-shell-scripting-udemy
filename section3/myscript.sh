b1d7d66e3879353decbb054fa945f1e5b2d08ac6
jspark <sylvan0212@gmail.com>
2021-08-25 16:36:30 +0900

    finished 41. Script Open done

--- section3/myscript.sh
+++ section3/myscript.sh
@@ -1,8 +1,15 @@
+# which bash
 #! /usr/bin/bash
 
 read -p "what is your name? " yourname
 
 echo "hello $yourname nice to meet you"
+
+echo "tell me the path of the file you want to open"
+read filepath
+
+code $filepath
+
 echo -----------------*
 
 # this is a comment for myself
