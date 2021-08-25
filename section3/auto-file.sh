1f68018108a502c15c664ee67536e293a800b8e5
jspark <sylvan0212@gmail.com>
2021-08-25 16:56:52 +0900

    finished section 3

---
+++ section3/auto-file.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+###### Automatically Created Script ########
--- section3/ex2-script.sh
+++ section3/ex2-script.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/bash
 
 read -p "name of the script: " scriptname
-touch ${scriptname}.sh
+touch ${scriptname}
+
+echo "#! /usr/bin/bash" >> ${scriptname}
+echo "###### Automatically Created Script ########" >> ${scriptname}
+cal
+
+chmod +x ${scriptname}
+
+echo "Done"
